0652f58986db0e3d15b5e595c5012bc348b670da
Audrey Williams <audreysfwilliams@gmail.com>
2022-06-20 13:02:23 +0100

    basic operation

--- script.js
+++ script.js
@@ -34,27 +34,69 @@ function operate(operator, a, b) {
             break;
     }
 }
-const testBtn = document.getElementById('=');
-const clearBtn = document.getElementById('clear');
 
-testBtn.addEventListener('click', () => {
-    operate('*', 2, 4);
-    console.log(operate('*', 2 , 4));
-})
+// const testBtn = document.getElementById('=');
+// testBtn.addEventListener('click', () => {
+//     operate('*', 2, 4);
+//     console.log(operate('*', 2 , 4));
+// })
 
-const firstValue = [];
-const secondValue = [];
+
+function clearDisplay() {
+    document.getElementById('screen').innerHTML = '';
+    currentValue.length = 0;
+}
+
+let firstValue = 0;
+let secondValue = 0;
+let currentValue = [];
+let operator = null;
+
+
+// function display(clicked) {
+//     if (currentValue.length > 7) {
+//         alert('Can\'t enter more than 8 digits');
+//     }
+//     else {
+//         currentValue.push(clicked);
+//         document.getElementById('screen').innerHTML = currentValue.join('');
+//         if (clicked == '+') {
+//             console.log(currentValue);
+//             operator = clicked;
+//             firstValue = currentValue;
+//             clearDisplay();
+//             console.log(firstValue);
+//         }
+//     }
+
+    
+// }
 
 function display(clicked) {
-    if (firstValue.length > 7) {
+    if (currentValue.length > 7) {
         alert('Can\'t enter more than 8 digits');
     }
-    else {
-        firstValue.push(clicked);
-        document.getElementById('screen').innerHTML = firstValue.join('');
+    else if (clicked == '+' || clicked == '-' || clicked == '*' || clicked == '/') {
+        operator = clicked;
+        firstValue = currentValue.join('');
+        clearDisplay();
+    }
+
+    else if (clicked == '=') {
+        secondValue = currentValue.join('');
+        clearDisplay();
+        document.getElementById('screen').innerHTML = operate(operator, Number(firstValue), Number(secondValue));
+    }
+
+    else if (clicked == '0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9') {
+        currentValue.push(clicked);
+        document.getElementById('screen').innerHTML = currentValue.join('');
     }
     
 }
+
+
+
 
 
 
