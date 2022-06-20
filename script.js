be9a66d724ada6f6de0c1f688ecd78d132948748
Audrey Williams <audreysfwilliams@gmail.com>
2022-06-20 13:53:50 +0100

    chaining operations

--- script.js
+++ script.js
@@ -47,45 +47,53 @@ function clearDisplay() {
     currentValue.length = 0;
 }
 
+function clearAll() {
+    document.getElementById('screen').innerHTML = '';
+    currentValue.length = 0;
+    firstValue = 0;
+    secondValue = 0;
+    operator = null;
+}
+
 let firstValue = 0;
 let secondValue = 0;
 let currentValue = [];
 let operator = null;
+let resultDone = false;
 
-
-// function display(clicked) {
-//     if (currentValue.length > 7) {
-//         alert('Can\'t enter more than 8 digits');
-//     }
-//     else {
-//         currentValue.push(clicked);
-//         document.getElementById('screen').innerHTML = currentValue.join('');
-//         if (clicked == '+') {
-//             console.log(currentValue);
-//             operator = clicked;
-//             firstValue = currentValue;
-//             clearDisplay();
-//             console.log(firstValue);
-//         }
-//     }
-
-    
-// }
 
 function display(clicked) {
     if (currentValue.length > 7) {
         alert('Can\'t enter more than 8 digits');
     }
     else if (clicked == '+' || clicked == '-' || clicked == '*' || clicked == '/') {
+        if (firstValue != 0) {
+            secondValue = currentValue.join('');
+            firstValue = operate(operator, Number(firstValue), Number(secondValue));
+            operator = clicked;
+            clearDisplay();
+        }
+        else if (firstValue == 0) {
         operator = clicked;
         firstValue = currentValue.join('');
         clearDisplay();
+        }
+        console.log(`current first value ${firstValue}`);
+        console.log(`current second value ${secondValue}`);
     }
 
     else if (clicked == '=') {
-        secondValue = currentValue.join('');
-        clearDisplay();
-        document.getElementById('screen').innerHTML = operate(operator, Number(firstValue), Number(secondValue));
+        if (resultDone == false) {
+            resultDone = true;
+            secondValue = currentValue.join('');
+            clearDisplay();
+            document.getElementById('screen').innerHTML = operate(operator, Number(firstValue), Number(secondValue));
+            console.log(`result ${operate(operator, Number(firstValue), Number(secondValue))}`);
+        }
+        else {
+            document.getElementById('screen').innerHTML = operate(operator, Number(firstValue), Number(secondValue));
+        }
+
     }
 
     else if (clicked == '0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9') {
